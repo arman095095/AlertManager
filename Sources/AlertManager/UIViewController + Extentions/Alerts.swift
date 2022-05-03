cdc3fb3b8799fae7d034f359448776395ca26a98
Арман Чархчян <re-mont1@mail.ru>
2022-05-03 06:18:38 +0400

    fix

--- Sources/AlertManager/UIViewController + Extentions/Alerts.swift
+++ Sources/AlertManager/UIViewController + Extentions/Alerts.swift
@@ -30,49 +30,83 @@ extension UIViewController {
             case alertMessage = "Вы действительно хотите выйти?"
             case acceptButtonTitle = "Выйти"
         }
+        
+        enum Delete: String {
+            case alertTitle = "Вы уверены?"
+            case alertMessage = "Хотите удалить этот пост?"
+            case acceptButtonTitle = "Удалить"
+        }
+    }
+    
+    public func showAlertDelete(acceptHandler: @escaping (() -> Void),
+                                denyHandler: @escaping (() -> Void)) {
+        showAlertDestructive(title: AlertStrings.Delete.alertTitle.rawValue, message: AlertStrings.Delete.alertMessage.rawValue, acceptButtonTitle: AlertStrings.Delete.acceptButtonTitle.rawValue, cancelButtonTitle: AlertStrings.cancelButtonTitle.rawValue, acceptHandler: acceptHandler,
+                             denyHandler: denyHandler)
     }
     
     public func showAlertForRecover(acceptHandler: @escaping (() -> Void),
                                     denyHandler: @escaping (() -> Void)) {
-        showAlert(title: AlertStrings.Recover.alertTitle.rawValue,
-                  message: AlertStrings.Recover.alertMessage.rawValue,
-                  acceptButtonTitle: AlertStrings.Recover.acceptButtonTitle.rawValue,
-                  cancelButtonTitle: AlertStrings.cancelButtonTitle.rawValue,
-                  acceptHandler: acceptHandler,
-                  denyHandler: denyHandler)
+        showAlertDefault(title: AlertStrings.Recover.alertTitle.rawValue,
+                         message: AlertStrings.Recover.alertMessage.rawValue,
+                         acceptButtonTitle: AlertStrings.Recover.acceptButtonTitle.rawValue,
+                         cancelButtonTitle: AlertStrings.cancelButtonTitle.rawValue,
+                         acceptHandler: acceptHandler,
+                         denyHandler: denyHandler)
     }
     
     public func showAlertForLogout(acceptHandler: @escaping (() -> Void),
                                    denyHandler: @escaping (() -> Void)) {
-        showAlert(title: AlertStrings.Logout.alertTitle.rawValue,
-                  message: AlertStrings.Logout.alertMessage.rawValue,
-                  acceptButtonTitle: AlertStrings.Logout.acceptButtonTitle.rawValue,
-                  cancelButtonTitle: AlertStrings.cancelButtonTitle.rawValue,
-                  acceptHandler: acceptHandler,
-                  denyHandler: denyHandler)
+        showAlertDestructive(title: AlertStrings.Logout.alertTitle.rawValue,
+                             message: AlertStrings.Logout.alertMessage.rawValue,
+                             acceptButtonTitle: AlertStrings.Logout.acceptButtonTitle.rawValue,
+                             cancelButtonTitle: AlertStrings.cancelButtonTitle.rawValue,
+                             acceptHandler: acceptHandler,
+                             denyHandler: denyHandler)
     }
     
     public func showAlertForRemove(acceptHandler: @escaping (() -> Void),
                                    denyHandler: @escaping (() -> Void)) {
-        showAlert(title: AlertStrings.Remove.alertTitle.rawValue,
-                  message: AlertStrings.Remove.alertMessage.rawValue,
-                  acceptButtonTitle: AlertStrings.Remove.acceptButtonTitle.rawValue,
-                  cancelButtonTitle: AlertStrings.cancelButtonTitle.rawValue,
-                  acceptHandler: acceptHandler,
-                  denyHandler: denyHandler)
+        showAlertDestructive(title: AlertStrings.Remove.alertTitle.rawValue,
+                             message: AlertStrings.Remove.alertMessage.rawValue,
+                             acceptButtonTitle: AlertStrings.Remove.acceptButtonTitle.rawValue,
+                             cancelButtonTitle: AlertStrings.cancelButtonTitle.rawValue,
+                             acceptHandler: acceptHandler,
+                             denyHandler: denyHandler)
     }
     
-    public func showAlert(title: String,
-                          message: String,
-                          acceptButtonTitle: String,
-                          cancelButtonTitle: String,
-                          acceptHandler: @escaping (() -> Void),
-                          denyHandler: @escaping (() -> Void)) {
+    public func showAlertDefault(title: String,
+                                 message: String,
+                                 acceptButtonTitle: String,
+                                 cancelButtonTitle: String,
+                                 acceptHandler: @escaping (() -> Void),
+                                 denyHandler: @escaping (() -> Void)) {
         let alert = UIAlertController(title: title,
                                       message: message,
                                       preferredStyle: .alert)
         let acceptAction = UIAlertAction(title: acceptButtonTitle,
                                          style: .default) { _ in
+            acceptHandler()
+        }
+        let cancelAction = UIAlertAction(title: cancelButtonTitle,
+                                         style: .default) { _ in
+            denyHandler()
+        }
+        alert.addAction(cancelAction)
+        alert.addAction(acceptAction)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    public func showAlertDestructive(title: String,
+                                     message: String,
+                                     acceptButtonTitle: String,
+                                     cancelButtonTitle: String,
+                                     acceptHandler: @escaping (() -> Void),
+                                     denyHandler: @escaping (() -> Void)) {
+        let alert = UIAlertController(title: title,
+                                      message: message,
+                                      preferredStyle: .alert)
+        let acceptAction = UIAlertAction(title: acceptButtonTitle,
+                                         style: .destructive) { _ in
             acceptHandler()
         }
         let cancelAction = UIAlertAction(title: cancelButtonTitle,
